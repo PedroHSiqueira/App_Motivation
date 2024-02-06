a68f30e6efab3f45fe4dc51490acf111ac2558d4
Pedro Siqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-02-06 14:52:45 -0300

    Feat: UserActivity layout

--- app/src/main/java/com/example/motivation/MainActivity.kt
+++ app/src/main/java/com/example/motivation/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.motivation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.motivation.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Esconder a barra de suporte
+        supportActionBar?.hide()
+
+        //eventos
+        binding.buttonNewPhrase.setOnClickListener(this)
     }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.button_new_phrase) {
+
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/example/motivation/UserActivity.kt
@@ -0,0 +1,43 @@
+package com.example.motivation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.motivation.databinding.ActivityMainBinding
+import com.example.motivation.databinding.ActivityUserBinding
+
+class UserActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityUserBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        binding.buttonSaveName.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.button_save_name) {
+            handleSave()
+        }
+    }
+
+    private fun handleSave() {
+        val username = binding.editName.text.toString()
+        val validationText = R.string.validation_mandatory_name
+
+        if (username != "") {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        } else {
+            Toast.makeText(this, validationText, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
